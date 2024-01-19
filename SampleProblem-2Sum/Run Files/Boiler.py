f3bdced3aeb6372f22459883bf11faf4ac22bf0c
Buggermenot <vermatanmay220@gmail.com>
2024-01-19 12:08:51 +0530

    Updated Boiler.py - Need Testing

--- SampleProblem-2Sum/Run Files/Boiler.py
+++ SampleProblem-2Sum/Run Files/Boiler.py
@@ -1,6 +1,25 @@
 import sys
 sys.path.append('..')
 from YourSolution import Solution
+from Test_Cases import tests
 
+def testAll(tests: list, verbose=False):
+    passCount = 0
+    failedOn = []
+    total_test = len(tests)
 
-print(Solution.twoSum([1, 2, 3], 3))
+    for i in range(total_test):
+        res = tests[i].test()
+        if res:
+            passCount += 1
+        else:
+            failedOn.append(i)
+    
+    print('Test Results:')
+    print(f'\tScore: {(passCount * 100) / total_test}')
+    print(f'\tPassed: {passCount} / {total_test}')
+    print(f'\tTests Failed on: {failedOn}')
+
+if __name__ == "__main__":
+    allTests = tests.Tests().loadTests(target_func=Solution.twoSum)
+    testAll(allTests)
